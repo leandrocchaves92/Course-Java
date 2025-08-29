38b4a88d95446f0814d1fdf34802e941af51fd33
leandrocchaves92 <leandro_c_chaves@yahoo.com.br>
2025-08-29 11:30:18 -0300

    Atualizando o Calculadobro

--- unidade3/CalculaDobro.java
+++ unidade3/CalculaDobro.java
@@ -1,16 +1,23 @@
 package unidade3;
 
-import java.util.Scanner;
+//import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class CalculaDobro {
 
 	public static void main(String[] args) {
-	
+		/*
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Digite o numero ");
 		double numero = teclado.nextDouble();
 		double dobro = numero * 2;
 		System.out.println("O dobro do numero é "+dobro);
+		*/
+		var numero = JOptionPane.showInputDialog("Digite o numero");
+		var dobro = Double.parseDouble(numero)*2;
+		
+		JOptionPane.showMessageDialog(null, "O dobro de seu numero é "+numero);
 		
 	}
 
