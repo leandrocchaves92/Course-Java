7a184f9c8a82fca22bc3726be57d9070ec6cbb16
leandrocchaves92 <leandro_c_chaves@yahoo.com.br>
2025-09-01 09:47:10 -0300

    Realizando algoritimo OpcoesdePagamento com  system.out no eclipse

--- unidade3/OpcoesPagamento.java
+++ unidade3/OpcoesPagamento.java
@@ -10,8 +10,10 @@ public class OpcoesPagamento {
 		double produto_avista = valor_produto * 0.9;
 		double produto_parcelado = valor_produto / 3;
 		
-		JOptionPane.showMessageDialog(null, "O valor do produto a vista é de: R$"+produto_avista+" E o valor do produto parcelado e´de R$:"+produto_parcelado);
+		//JOptionPane.showMessageDialog(null, "O valor do produto a vista é de: R$"+produto_avista+" E o valor do produto parcelado e´de R$:"+produto_parcelado);
 		
+		System.out.println("O produto pago a vista tem 10% de desconto e sairá por: R$ "+produto_avista);
+		System.out.println("O produto pago a prazo em 3x de R$ "+produto_parcelado);
 		
 	}
 
