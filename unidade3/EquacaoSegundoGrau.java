e7d84531926d65fd5977078896f7998555a5ac73
leandrocchaves92 <leandro_c_chaves@yahoo.com.br>
2025-09-01 18:27:00 -0300

    Acerto nas formulas da raiz quadrada

--- unidade3/EquacaoSegundoGrau.java
+++ unidade3/EquacaoSegundoGrau.java
@@ -20,8 +20,8 @@ public class EquacaoSegundoGrau {
 		
 		delta = b*b -4*a*c;
 		if(delta >=0);
-			x1 = -b -(Math.sqrt(delta))/2*a;
-			x2 = -b +(Math.sqrt(delta))/2*a;
+			x1 = (-b -(Math.sqrt(delta)))/2*a;
+			x2 = (-b +(Math.sqrt(delta)))/2*a;
 			System.out.println("O valor de X1 "+x1);
 			System.out.println("O valor de X2 "+x2);
 		
